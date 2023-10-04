653ad26856653b6daa7e6a25029a68e6ba164c0b
millyMilly1 <chukwurahenry@gmail.com>
2023-10-04 17:56:59 +0100

    mailer php

--- mailer.php
+++ mailer.php
@@ -5,8 +5,15 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+$name = $_POST['name'];
+ $email = $_POST['email'];
+ $message = $_POST['message'];
+
+//Load Composer's
+require 'PHPMailer/src/Exception.php';
+require  'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -15,24 +22,26 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
+    $mail->Username   = 'sapphires451@gmail.com';                     //SMTP username
+    $mail->Password   = 'pqqwicahnhkehmbe';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->setFrom('tebc@example.com', 'TEBC');
+    $mail->addAddress('chukwurahenry@gmail.com', 'milly');     //Add a recipient
     $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addAddress('swiftXchange3@gamil.com', 'me');     //Add a recipient
+    $mail->addAddress('lillianchukwura@gmail.com');               //Name is optional
     $mail->addReplyTo('info@example.com', 'Information');
     $mail->addCC('cc@example.com');
     $mail->addBCC('bcc@example.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
@@ -42,6 +51,7 @@ try {
 
     $mail->send();
     echo 'Message has been sent';
+    header("Location: thank-you.html?mailsend");
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
