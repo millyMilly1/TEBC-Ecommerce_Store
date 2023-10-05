51f9a6390be3b31209a9d112cc47d4b84d1f3bfc
millyMilly1 <chukwurahenry@gmail.com>
2023-10-05 11:06:11 +0100

    mailer.php section

--- mailer.php
+++ mailer.php
@@ -45,8 +45,8 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'contact-form';
+    $mail->Body    = 'this message is from' . $name .'<br> sent from' . $email . '\n'. $message;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
